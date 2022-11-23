aabf38042b7626f08546f29b910345545e1dd229
GinamirXD <ginamir04@gmail.com>
2022-11-23 13:58:19 +0300

    нева

---
+++ qwx/ComplexNumber.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qwx
+{
+    internal class ComplexNumber
+    {
+        public double RealNumber{ get; set; }
+        public double ImaginaryPart { get; set; }
+        public double Modul { get; set; }
+        public double Angle { get; set; }
+
+        public ComplexNumber(double realNumber, double imaginaryPart)
+        {
+            RealNumber = realNumber;
+            ImaginaryPart = imaginaryPart;
+            Modul = Math.Sqrt(realNumber * realNumber + imaginaryPart * imaginaryPart);
+            Angle = Math.Atan(imaginaryPart / realNumber);
+        }
+
+        public ComplexNumber(double realNumber):this(realNumber, 0) { }
+
+        public ComplexNumber() : this(0, 0) { }
+
+        public override string ToString()
+        {
+            if (RealNumber == 0)
+                return $"{ImaginaryPart}i";
+            else if (ImaginaryPart == 0)
+                return $"{RealNumber}";
+            else if (Math.Abs(ImaginaryPart) == 1)
+                return (ImaginaryPart > 0) ? $"{RealNumber}+i" : $"{RealNumber}-i";
+            else
+                return (ImaginaryPart > 0) ? $"{RealNumber}+{ImaginaryPart}i" : $"{RealNumber}-{Math.Abs(ImaginaryPart)}i";
+        }
+
+        public ComplexNumber Add(ComplexNumber number)
+        {
+            var newRealNumber = RealNumber + number.RealNumber;
+            var newImaginaryPart = ImaginaryPart + number.ImaginaryPart;
+            return new ComplexNumber(newRealNumber, newImaginaryPart);
+        }
+
+        public ComplexNumber Minus(ComplexNumber number)
+        {
+            var newRealNumber = RealNumber - number.RealNumber;
+            var newImaginaryPart = ImaginaryPart - number.ImaginaryPart;
+            return new ComplexNumber(newRealNumber, newImaginaryPart);
+        }
+
+        public ComplexNumber Multiplication(ComplexNumber number)
+        {
+            var newRealNumber = RealNumber * number.RealNumber - ImaginaryPart * number.ImaginaryPart;
+            var newImaginaryPart = RealNumber * number.ImaginaryPart + number.RealNumber * ImaginaryPart;
+            return new ComplexNumber(newRealNumber, newImaginaryPart);
+        }
+
+        public ComplexNumber Div(ComplexNumber number)
+        {
+            var newRealNumber = (RealNumber * number.RealNumber + ImaginaryPart * number.ImaginaryPart) /
+                (number.RealNumber * number.RealNumber + number.ImaginaryPart * number.ImaginaryPart);
+            var newImaginaryPart = (number.RealNumber * ImaginaryPart - RealNumber*number.ImaginaryPart) /
+                (number.RealNumber * number.RealNumber + number.ImaginaryPart * number.ImaginaryPart);
+            return new ComplexNumber(newRealNumber, newImaginaryPart);
+        }
+        
+    }
+}
--- qwx/Program.cs
+++ qwx/Program.cs
@@ -1,29 +1,9 @@
-﻿static string GetMinX(int a, int b, int c)
-{
-    int D = b * b - 4 * a * c;
-    var x1 = (int)(-b + Math.Sqrt(D)) / 2 * a;
-    var x2 = (int)(-b - Math.Sqrt(D)) / 2 * a;
-    
-    if ((D < 0) || (a == 0 && b ==0))
-    {
-        return "Impossible";
-    }
-    else
-    {
-        if (x1 < x2)
-        {
-            return x1.ToString();
-        }
-        else
-        {
-            return x2.ToString();
-        }
-    }
-}
+﻿using qwx;
 
-Console.WriteLine(GetMinX(1, 2, 3));
-Console.WriteLine(GetMinX(0, 3, 2));
-Console.WriteLine(GetMinX(1, -2, -3));
-Console.WriteLine(GetMinX(5, 2, 1));
-Console.WriteLine(GetMinX(4, 3, 2));
-Console.WriteLine(GetMinX(0, 4, 5));
+var n1 = new ComplexNumber(1, 1);
+var n2 = new ComplexNumber(7, -6);
+var n3 = new ComplexNumber(3, 1);
+
+Console.WriteLine(n1.Angle);
+Console.WriteLine(Math.PI / 4);
+Console.WriteLine(n1.Modul);
