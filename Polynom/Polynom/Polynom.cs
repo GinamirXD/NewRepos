019fff370a5427ab0152acd094cb4d1a7a54cd87
GinamirXD <ginamir04@gmail.com>
2023-03-27 18:46:07 +0300

    text

---
+++ Polynom/Polynom/Polynom.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Polynomial
+{
+    private readonly List<double> Coefficients;
+
+    public Polynomial()
+    {
+        Coefficients = new List<double>();
+    }
+
+    public Polynomial(List<double> coefficients)
+    {
+        Coefficients = coefficients;
+    }
+
+    public static Polynomial BuildPolynomial(string filePath)
+    {
+        var lines = File.ReadAllLines(filePath);
+        var coefficients = new List<double>();
+        
+        foreach (var line in lines)
+        {
+            var parts = line.Split(' ');
+            if (parts.Length != 2)
+            {
+                throw new FormatException($"Invalid line format: {line}");
+            }
+            
+            if (!double.TryParse(parts[0], out var coefficient))
+            {
+                throw new FormatException($"Invalid coefficient: {parts[0]}");
+            }
+            
+            if (!int.TryParse(parts[1], out var degree))
+            {
+                throw new FormatException($"Invalid degree: {parts[1]}");
+            }
+            
+            while (coefficients.Count <= degree)
+            {
+                coefficients.Add(0);
+            }
+            
+            coefficients[degree] = coefficient;
+        }
+        
+        return new Polynomial(coefficients);
+    }
+
+    public override string ToString()
+    {
+        var terms = new List<string>();
+
+        for (var i = Coefficients.Count - 1; i >= 0; i--)
+        {
+            if (Coefficients[i] == 0)
+            {
+                continue;
+            }
+
+            var term = "";
+            if (i > 1)
+            {
+                term += $"{Coefficients[i]:0.##}x^{i}";
+            }
+            else if (i == 1)
+            {
+                term += $"{Coefficients[i]:0.##}x";
+            }   
+            else
+            {
+                term += $"{Coefficients[i]:0.##}";
+            }
+            terms.Add(term);
+        }
+
+        return string.Join(" + ", terms);
+    }
+
+    public void Insert(double coefficient, int degree)
+    {
+        if (degree < 0)
+        {
+            throw new ArgumentException("Degree must be non-negative.", nameof(degree));
+        }
+
+        while (Coefficients.Count <= degree)
+        {
+            Coefficients.Add(0);
+        }
+
+        var currentCoefficient = Coefficients[degree];
+        Coefficients[degree] = currentCoefficient + coefficient;
+
+        while (Coefficients.Count > 1 && Coefficients[Coefficients.Count - 1] == 0)
+        {
+            Coefficients.RemoveAt(Coefficients.Count - 1);
+        }
+    }
+
+
+    public void Sum(Polynomial other)
+    {
+        if (other == null)
+        {
+            throw new ArgumentNullException(nameof(other));
+        }
+
+        int degree = Math.Max(Coefficients.Count, other.Coefficients.Count);
+        for (int i = 0; i < degree; i++)
+        {
+            double coeff1 = (i < Coefficients.Count) ? Coefficients[i] : 0;
+            double coeff2 = (i < other.Coefficients.Count) ? other.Coefficients[i] : 0;
+            Coefficients[i] = coeff1 + coeff2;
+        }
+
+        while (Coefficients.Count > 1 && Coefficients[Coefficients.Count - 1] == 0)
+        {
+            Coefficients.RemoveAt(Coefficients.Count - 1);
+        }
+    }
+
+    public void Derive()
+    {
+        if (Coefficients.Count <= 1)
+        {
+            Coefficients.Clear();
+            Coefficients.Add(0);
+        }
+        else
+        {
+            for (int i = 1; i < Coefficients.Count; i++)
+            {
+                Coefficients[i - 1] = i * Coefficients[i];
+            }
+            Coefficients.RemoveAt(Coefficients.Count - 1);
+        }
+    }
+
+    public double Value(int x)
+    {
+        double result = 0;
+        for (int i = Coefficients.Count - 1; i >= 0; i--)
+        {
+            result = Coefficients[i] + (x * result);
+        }
+        return result;
+    }
+
+}
---
+++ Polynom/Polynom/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Polynom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var polynomialList1 = Polynomial.BuildPolynomial("C:\\Users\\ginam\\source\\repos\\NewRepos\\Polynom\\Polynom\\Polynom1.txt");
+            Console.WriteLine(polynomialList1);
+
+            polynomialList1.Insert(2, 4);
+            Console.WriteLine(polynomialList1);
+
+            Console.WriteLine("**********");
+            var polynomialList2 = Polynomial.BuildPolynomial("C:\\Users\\ginam\\source\\repos\\NewRepos\\Polynom\\Polynom\\Poynom2.txt");
+            Console.WriteLine(polynomialList2);
+
+            polynomialList1.Sum(polynomialList2);
+            Console.WriteLine(polynomialList1);
+
+            polynomialList2.Derive();
+            Console.WriteLine(polynomialList2);
+
+            var result = polynomialList2.Value(1);
+            var result2 = polynomialList2.Value(5);
+
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+
+            polynomialList1.DeleteOdd();
+            Console.WriteLine(polynomialList1);
+        }
+    }
+}
