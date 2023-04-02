41d239201181a65535588ed4c58f57c6eb0d9eb1
GinamirXD <ginamir04@gmail.com>
2023-04-02 23:02:42 +0300

    семестровка

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Graph
+{
+    private int V; // Кол-во вершин
+    private List<int>[] adj; // Список смежностей
+
+    Graph(int v)
+    {
+        V = v;
+        adj = new List<int>[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new List<int>();
+    }
+
+    // Функция для добавления ребра в граф
+    void addEdge(int v, int w) { adj[v].Add(w); }
+
+    // Рекурсивная функция для печати DFS, начиная с v
+    void DFSUtil(int v, bool[] visited)
+    {
+
+        //  Отмечает текущий узел как посещенный
+        visited[v] = true;
+        Console.Write(v + " ");
+
+        //Повторяется для всех вершин, смежных с этой вершиной
+        List<int> i = adj[v];
+        foreach (var n in i)
+        {
+            if (!visited[n])
+                DFSUtil(n, visited);
+        }
+    }
+
+    // Функция, которая возвращает транспонированный граф
+    Graph getTranspose()
+    {
+        Graph g = new Graph(V);
+        for (int v = 0; v < V; v++)
+        {
+
+            //Повторяется для всех вершин, смежных с этой вершиной
+            foreach (int i in adj[v])
+                g.addEdge(i, v);
+        }
+        return g;
+    }
+
+    void fillOrder(int v, bool[] visited, Stack<int> stack)
+    {
+        visited[v] = true;
+
+        foreach (var n in adj[v])
+        {
+            if (!visited[n])
+                fillOrder(n, visited, stack);
+        }
+
+        stack.Push(v);
+    }
+
+    void printSCCs()
+    {
+        Stack<int> stack = new Stack<int>();
+        var iterations = 0;
+        // Отмечает все вершины как не посещенные (для первого DFS)
+        var visited = new bool[V];
+
+        // Заполняет вершины в стеке в соответствии со временем их завершения
+        for (int i = 0; i < V; i++)
+        {
+            if (visited[i] == false)
+                fillOrder(i, visited, stack);
+            iterations++;
+        }
+
+        Graph gr = getTranspose();
+
+        for (int i = 0; i < V; i++)
+            visited[i] = false;
+
+        while (stack.Count != 0)
+        {
+
+            int v = stack.Pop();
+
+            //Выводит сильно связанный компонент выделенной вершины
+            if (visited[v] == false)
+            {
+                gr.DFSUtil(v, visited);
+                Console.WriteLine();
+            }
+            iterations++;
+        }
+        Console.WriteLine($"кол-во итераций - {iterations}");
+    }
+   
+    public static void Main(String[] args)
+    {
+        string[] lines = File.ReadAllLines("C:\\Users\\ginam\\source\\repos\\NewRepos\\GraphGenerator\\GraphGenerator\\graph50.txt");
+        int numVertices = int.Parse(lines[0]);
+        
+        Graph g = new Graph(numVertices);
+
+        List<List<int>> adjacencyList = new List<List<int>>();
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            string[] parts = lines[i].Split(' ');
+            int startVertex = int.Parse(parts[0]);
+            int endVertex = int.Parse(parts[1]);
+            List<int> edge = new List<int> { startVertex, endVertex };
+            adjacencyList.Add(edge);
+        }
+
+        for (int i = 0; i < adjacencyList.Count; i++)
+        {
+            g.addEdge(adjacencyList[i][0], adjacencyList[i][1]);
+        }
+
+        g.printSCCs();
+    }
+}
